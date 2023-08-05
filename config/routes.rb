e49324159918d4314ab320ad60060e880f82732a
Riccardo Giomi <riccardo.giomi@betterplace.org>
2023-08-05 18:13:01 +0200

    Make the Todo list page the application root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :todos
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'todos#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :todos
 end
