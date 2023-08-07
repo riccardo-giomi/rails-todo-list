30a0d80cad1dbbc944ea1deaaf4765ea0aaf45b5
Riccardo Giomi <riccardo.giomi@betterplace.org>
2023-08-07 19:17:04 +0200

    Add popovers to all buttons.
    Downloaded and pinned version 4.0.0 of
    [tailwindcss-stimulus-components](https://github.com/excid3/tailwindcss-stimulus-components/tree/master).
    Configured and used the Popover controller it provides.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
+
+// import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+import { Popover } from "tailwindcss-stimulus-components";
+application.register("popover", Popover);
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin 'tailwindcss-stimulus-components', to: 'tailwindcss-stimulus-components.module.js' # @4.0.0
 pin_all_from 'app/javascript/controllers', under: 'controllers'
